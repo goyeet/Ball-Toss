0ecfaaab14a43a5c77a46088597f4e507aa1b7e6
daskrys <caperezl@ucsc.edu>
2023-11-02 21:51:48 -0700

    basic ball phys added

--- docs/Prototype-2/main.js
+++ docs/Prototype-2/main.js
@@ -5,17 +5,34 @@ description = `
 [Release] Jumps
 `;
 
-characters = [];
+characters = [
+
+  `yylyy
+ lyylyyl
+yylylylyy
+yylylylyy
+yylllllyy
+yylylylyy
+yylylylyy
+ lyylyyl
+  yylyy`
+
+];
+
+const xBounds = 200;
+const yBounds = 100;
 
 options = {
-  viewSize: { x: 200, y: 100 },
+  viewSize: { x: xBounds, y: yBounds },
   isPlayingBgm: true,
   isReplayEnabled: true,
   seed: 2,
+  theme: "shapeDark",
+
+  isCapturing: true,
 };
 
-
-let p, v;
+let player, v;
 let isJumping;
 let angle;
 let width;
@@ -23,43 +40,67 @@ let space;
 let scr;
 let playerCollision;
 
+const groundX = 0;
+const groundY = 90; 
+const groundWidth = 300; 
+const groundHeight = 10; 
+
+function spawn() {
+  player = vec(10, 85);
+}
+
+function angleAdjust(angle) {
+  
+  angle -= 0.015;
+
+  return angle;
+}
+
 function update() {
   if (!ticks) {
-    p = vec(50, 85);
+    spawn();
     isJumping = angle = width = space = 0;
   }
+
   if (width + space < 0) {
     width = 200;
     space = rnd(50, 150);
   }
+
   color("blue");
-  rect(0, 90, width, 9);
-  rect(width + space, 90, 200, 9);
-  color("green");
-  playerCollision = box(p, 9, 9);
-  if (p.x < 0 || p.y > 99) {
+  rect(groundX, groundY, groundWidth, groundHeight);
+  
+  color("yellow");
+  playerCollision = char("a", player);;
+  //playerCollision = box(player, 9, 9);
+
+  if (player.x < 0 || player.y > yBounds || player.x > xBounds) {
+    setTimeout(spawn, 1000); // respawns after 1 second
     play("lucky");
-    end();
+    //end(); // ends
   }
+
   if (isJumping) {
-    p.add(v);
+    player.add(v);
     v.y += 0.1;
     if (playerCollision.isColliding.rect.blue) {
       isJumping = angle = 0;
-      p.y = 85;
+      player.y = 85;
     }
   } else {
     if (input.isPressed) {
-      bar(p, 20, 3, (angle -= 0.05), 0);
+      bar(player, 20, 1.25, (angle -= 0.05), 0); // this for aiming
+      //bar(player, 20, 3, angleAdjust(), 0); // this for aiming
     }
+
     if (input.isJustReleased) {
       play("jump");
       isJumping = 1;
       v = vec(4).rotate(angle);
     }
   }
-  p.x -= scr = clamp(p.x - 50, 0, 99) * 0.1 + difficulty;
-  width -= scr;
-  score += scr;
+
+  scr = clamp(player.x - 50, 0, 99) * 0.1 + difficulty;
+  //score += scr; controls score, for later
 }
 
--- docs/ref_collision/main.js
+++ docs/ref_collision/main.js
@@ -14,6 +14,7 @@ llllll
 `
 ];
 
+
 options = {
   isShowingScore: false
 };
